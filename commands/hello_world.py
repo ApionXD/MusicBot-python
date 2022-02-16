028c2ed0d3bdd7ead17fce9d1da60e737d82aa80
ApionXD <masonbell09@gmail.com>
2022-02-16 14:19:22 -0500

    Added ABSOLUTELY BAREBONES stuff to get commands working.

---
+++ commands/hello_world.py
@@ -0,0 +1,10 @@
+import commands.command
+from commands import command
+
+
+class HelloWorld(command.Command):
+    def __init__(self):
+        None
+
+    async def run_command(self, command_event):
+        await command_event.message.channel.send("Hello world!")
