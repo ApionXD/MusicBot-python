8b1c41401949ed9881aec4d9b168c32b94a8e987
kunle7894 <kunle54321@hotmail.com>
2022-04-14 16:12:16 -0400

    Made show what is currently playing. Add songs to queue show what was added. Doesn't show song Now Playing: when moving to new song in queue needs to be done.

--- src/commands/play.py
+++ src/commands/play.py
@@ -12,21 +12,26 @@ class Play(Command):
         voice_channel = discord.utils.find(lambda m: m.id == command_event.guild_settings.voice_channel_id, server.voice_channels)
         channel = server.id
         if (channel in song_list): #If play is called automatically goes to top(easier for testing not final)
-            song_list[channel].insert(0, command_event.words[1:])
+            source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
+            song_list[channel].insert(0, source)
         else:
-            song_list[channel] = command_event.words[1:]
+            source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
+            song_list[channel] = [source]
 
         if server.voice_client==None or not server.voice_client.is_connected(): #Sees if already in voice
             connection = await voice_channel.connect()
-            self.check_songs(command_event, connection, server)
+            await command_event.message_event.channel.send("Now Playing: "+self.check_songs(command_event, connection, server).url)
         else:
             await command_event.message_event.channel.send( #Perhaps a way to make queue share this
-                "Added to queue")
+                "Added to queue"+" "+song_list[channel][-1].url)
 
 
     def check_songs(self, command_event, connection, server): #Keeps playing songs while there is a queue
         channel = server.id
         if song_list[channel] != [] and not server.voice_client==None and server.voice_client.is_connected():
-            source = YTDLSource.YTSource(" ".join(song_list[channel][0]))
+            print(song_list[channel])
+            source = song_list[channel][0]
+            connection.play(source,  after= lambda x=None: command_event.message_event.channel.send(self.check_songs(command_event, connection, server)))
             song_list[channel].pop(0)
-            connection.play(source, after=lambda x=None: self.check_songs(command_event, connection, server))
+            return source
+        return
--- src/commands/queue.py
+++ src/commands/queue.py
@@ -7,6 +7,7 @@ class Queue(Command):
     async def run_command(self, command_event):
         server = command_event.message_event.guild
         #voice_channel = [x for x in server.voice_channels if x.id == command_event.guild_settings.voice_channel_id][0]
-        song_list[server.id].append(command_event.words[1:])
+        source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
+        song_list[server.id].append(source)
         await command_event.message_event.channel.send(
-            "Added to queue")
+            "Added to queue"+" "+song_list[server.id][-1].url)
