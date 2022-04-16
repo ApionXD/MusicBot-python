c0a69e1f8b63d57345843006d9cee149a6d0bcad
ApionXD <apion@apionservers.com>
2022-04-16 17:55:34 -0400

    Added nice looking embed for Now Playing

--- src/commands/play.py
+++ src/commands/play.py
@@ -1,41 +1,46 @@
 import discord
+import asyncio
+
+import bot_container
 
 from commands.command import Command
 from sources import YTDLSource
 
-song_list = {} #May need to move somewhere more central
-cur_source = {}
+song_map = {} #May need to move somewhere more central
+
 
 class Play(Command):
     perm_level = 1000
     async def run_command(self, command_event):
         server = command_event.message_event.guild
-        voice_channel = discord.utils.find(lambda m: m.id == command_event.guild_settings.voice_channel_id, server.voice_channels)
-        channel = server.id
-        if (channel in song_list): #If play is called automatically goes to top(easier for testing not final)
-            source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
-            song_list[channel].insert(0, source)
+        guild_id = server.id
+        if (guild_id in song_map):
+            song_map[guild_id].append(YTDLSource.YTSource(" ".join(command_event.words[1:])))
         else:
-            source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
-            song_list[channel] = [source]
-            cur_source[channel] = song_list[channel]
-
-        if server.voice_client==None or not server.voice_client.is_connected(): #Sees if already in voice
-            connection = await voice_channel.connect()
-            await command_event.message_event.channel.send("Now Playing: "+self.check_songs(command_event, connection, server).url)
+            song_map[guild_id] = [YTDLSource.YTSource(" ".join(command_event.words[1:]))]
+        if server.voice_client is None:
+            await discord.utils.find(lambda m: m.id == command_event.guild_settings.voice_channel_id, server.voice_channels).connect()
         else:
-            await command_event.message_event.channel.send( #Perhaps a way to make queue share this
-                "Added to queue"+" "+song_list[channel][0].url)
+            server.voice_client.disconnect()
+        self.play_next_song(server, command_event.guild_settings.command_channel_id )
 
+    # Plays next song
+    def play_next_song(self, server, channel_id):
+        id = server.id
+        # If there is a queue
+        if len(song_map[id]) > 0:
+            # Sets source to be the first song in queue
+            source = song_map[id][0]
+            song_map[id].pop(0)
+            text_channel = discord.utils.find(lambda m: m.id == channel_id, server.text_channels)
+            embed = discord.Embed(title="Now Playing", url=source.data['uploader_url'])
+            embed.set_image(url=source.data['thumbnails'][0]['url'])
+            embed.set_author(name="MusicBot")
+            embed.add_field(name="Artist", value=source.data['artist'], inline=True)
+            embed.add_field(name="Track", value=source.data['track'], inline=True)
+            embed.add_field(name="Duration", value=f"{int(source.data['duration'] / 60)}:{source.data['duration'] % 60}", inline=True)
+            asyncio.run_coroutine_threadsafe(text_channel.send(embed=embed), bot_container.bot_instance.loop)
+            server.voice_client.play(source, after=lambda x=None: self.play_next_song(server, channel_id))
+        else:
+            asyncio.run_coroutine_threadsafe(server.voice_client.disconnect(), bot_container.bot_instance.loop)
 
-    def check_songs(self, command_event, connection, server): #Keeps playing songs while there is a queue
-        channel = server.id
-        if song_list[channel] != [] and not server.voice_client==None and server.voice_client.is_connected():
-            print(song_list[channel])
-            cur_source[channel] = song_list[channel][0]
-            connection.play(cur_source[channel],  after= lambda x=None: self.check_songs(command_event, connection, server))
-            cur_source[channel] = song_list[channel][0]
-            song_list[channel].pop(0)
-            print(cur_source[channel].url)
-            return cur_source[channel]
-        return
--- src/python_client.py
+++ src/python_client.py
@@ -20,30 +20,31 @@ class PythonClient(discord.Client):
 
 
     async def on_message(self, message_event):
-        settings = self.settings_map[message_event.guild.id]
-        prefix = settings.prefix
-        command_channel_id = settings.command_channel_id
-        # Checks to see if the message is a bot command
-        # This will need to check the guild settings for the prefix
-        # Also need to add exceptions for when a message references a command that isn't registered
-        if message_event.channel.id == command_channel_id and message_event.content[0] == prefix:
-            words = message_event.content[1:].split()
-            # The first word of the message, which should be the name of the command
-            command_name = words[0]
-            # The CommandEvent object, which stores stuff relevant to the CURRENT call of the command
-            # Should probably add words to this object
-            new_command_event = command_event.CommandEvent(message_event, self.settings_map[message_event.guild.id])
-            # Gets the previously registered command by name
-            command = self.command_map[command_name]
-            if not command:
-                await message_event.channel.send_message("Command not found!")
-                return
-            # Runs the command
-            if settings.perm_map.get_highest_permission_level(message_event.author) >= command.perm_level:
-                await command.run_command(new_command_event)
-            else:
-                await message_event.channel.send("You don't have permission to run this command! "
-                                                 "You have permission level "
-                                                 f"{settings.perm_map.get_highest_permission_level(message_event.author)}"
-                                                 f" but the command requires {command.perm_level}")
+        if not message_event.author.bot:
+            settings = self.settings_map[message_event.guild.id]
+            prefix = settings.prefix
+            command_channel_id = settings.command_channel_id
+            # Checks to see if the message is a bot command
+            # This will need to check the guild settings for the prefix
+            # Also need to add exceptions for when a message references a command that isn't registered
+            if message_event.channel.id == command_channel_id and message_event.content[0] == prefix:
+                words = message_event.content[1:].split()
+                # The first word of the message, which should be the name of the command
+                command_name = words[0]
+                # The CommandEvent object, which stores stuff relevant to the CURRENT call of the command
+                # Should probably add words to this object
+                new_command_event = command_event.CommandEvent(message_event, self.settings_map[message_event.guild.id])
+                if not command_name in self.command_map:
+                    await message_event.channel.send_message("Command not found!")
+                    return
+                # Gets the previously registered command by name
+                command = self.command_map[command_name]
+                # Runs the command
+                if settings.perm_map.get_highest_permission_level(message_event.author) >= command.perm_level:
+                    await command.run_command(new_command_event)
+                else:
+                    await message_event.channel.send("You don't have permission to run this command! "
+                                                     "You have permission level "
+                                                     f"{settings.perm_map.get_highest_permission_level(message_event.author)}"
+                                                     f" but the command requires {command.perm_level}")
 
