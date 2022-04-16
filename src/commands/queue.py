de8f535b58a1cb7266c068c315ad3df98f05238f
ApionXD <apion@apionservers.com>
2022-04-16 17:55:34 -0400

    Added nice looking embed for Now Playing

--- src/commands/queue.py
+++ src/commands/queue.py
@@ -1,5 +1,5 @@
 from commands.command import Command
-from commands.play import song_list, Play
+from commands.play import song_map
 from sources import YTDLSource
 
 
@@ -7,23 +7,8 @@ class Queue(Command):
     perm_level = 1000
     async def run_command(self, command_event):
         server = command_event.message_event.guild
-        voice_channel = [x for x in server.voice_channels if x.id == command_event.guild_settings.voice_channel_id][0]
-        if (len(command_event.words)>1):
-            if len(song_list[server.id])==0 and (
-                    server.voice_client is None or (server.voice_client is not None and not server.voice_client.is_playing())):
-                Play.run_command(command_event)
-            else:
-                source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
-                song_list[server.id].append(source)
-                await command_event.message_event.channel.send(
-                "Added to queue"+" "+song_list[server.id][-1].url)
-        else:
-            if len(song_list[server.id])>0:
-                await command_event.message_event.channel.send(
-                    ("Printing out the current queue"))
-                for i in range(len(song_list[server.id])):
-                    await command_event.message_event.channel.send(
-                        (str(i+1)+": "+song_list[server.id][i].url))
-            else:
-                await command_event.message_event.channel.send(
-                    ("The queue is empty"))
+        #voice_channel = [x for x in server.voice_channels if x.id == command_event.guild_settings.voice_channel_id][0]
+        source = YTDLSource.YTSource(" ".join(command_event.words[1:]))
+        song_map[server.id].append(source)
+        await command_event.message_event.channel.send(
+            "Added to queue" +" " + song_map[server.id][-1].url)
