ae4addce72bd2506cd17a3c4591b99b2566c703f
ApionXD <apion@apionservers.com>
2022-04-16 19:29:25 -0400

    Consolidated the logic that determines when the bot joins a voice channel into one method

--- src/commands/queue.py
+++ src/commands/queue.py
@@ -1,5 +1,7 @@
+import discord
+
 from commands.command import Command
-from commands.play import song_map
+from commands.play import song_map, play_next_song
 from sources import YTDLSource
 
 
@@ -13,7 +15,17 @@ class Queue(Command):
             song_map[server.id].append(source)
         else:
             song_map[server.id] = [source]
-
-        await command_event.message_event.channel.send(
-            "Added to queue" +" " + song_map[server.id][-1].url)
+            play_next_song(server, command_event.guild_settings.command_channel_id, command_event.guild_settings.voice_channel_id)
+        embed = discord.Embed(title="Added to queue", url=source.data['uploader_url'])
+        embed.set_image(url=source.data['thumbnails'][0]['url'])
+        embed.set_author(name="MusicBot")
+        if 'artist' in source.data:
+            embed.add_field(name="Artist", value=source.data['artist'], inline=True)
+        if 'track' in source.data:
+            embed.add_field(name="Track", value=source.data['track'], inline=True)
+        if 'duration' in source.data:
+            embed.add_field(name="Duration",
+                            value=f"{int(source.data['duration'] / 60)}:{source.data['duration'] % 60}",
+                            inline=True)
+        await command_event.message_event.channel.send(embed=embed)
 
