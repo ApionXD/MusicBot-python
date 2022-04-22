e19c88970071d86367204773ed8a8887094ab960
Kyle Iarrobino <khi18@my.fsu.edu>
2022-04-22 13:39:13 -0400

    merge

--- src/command_map.py
+++ src/command_map.py
@@ -1,6 +1,10 @@
 import queue
 
+<<<<<<< Updated upstream
 from commands import hello_world, play, set_prefix, leave, queue, get_perm, help, set_perm, set_permu, resume, pause
+=======
+from commands import hello_world, play, set_prefix, leave, queue, get_perm, skip, help, set_perm, set_permu, resume, pause
+>>>>>>> Stashed changes
 
 command_map = {
     "hello": hello_world.HelloWorld(),
@@ -9,6 +13,10 @@ command_map = {
     "leave": leave.Leave(),
     'queue': queue.Queue(),
     'getperms': get_perm.GetPerm(),
+<<<<<<< Updated upstream
+=======
+    'skip': skip.Skip()
+>>>>>>> Stashed changes
     'setpermr': set_perm.SetPerm(),
     'setpermu': set_permu.SetPermu(),
     'help': help.Help(),
